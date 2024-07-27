52dae8ecac8f5e99f1c9e2fcf7805a9d1949f5fb
DamirWork <damir.gilmanov2015@yandex.ru>
2024-07-27 14:56:27 +0500

    270724

---
+++ prj_for_yandex/Scenes/Main/Main.gd
@@ -0,0 +1,92 @@
+extends Control
+
+func _ready():
+	$d2.disabled=Global.Milina_disabled
+	$d3.disabled=Global.Jade_disabled
+	$d4.disabled=Global.Kitana_disabled
+	$d5.disabled=Global.Sindel_disabled
+	$d6.disabled=Global.Tanya_disabled
+	$d7.disabled=Global.Frost_disabled
+	$d8.disabled=Global.Cetrion_disabled
+	$d9.disabled=Global.Skarlet_disabled
+	$d10.disabled=Global.Ferra_disabled
+	
+	Global.connect('foc_unfoc',self,'_foc_unfoc')
+	Global.js_foc_unfoc()
+	
+	Global.js_game_ready_api()
+	
+	Global.connect("ad",self,'_ad')
+	Global.js_show_ad()
+	get_tree().paused=true
+	
+func _foc_unfoc(value):
+	if value=="foc":
+		get_tree().paused=false
+	elif value=="unfoc":
+		get_tree().paused=true
+
+func _ad(value):
+	get_tree().paused=false
+
+func _on_d1_pressed():
+	$Select.play()
+	$AnimationPlayer.play("Sonya")
+
+func _on_d2_pressed():
+	$Select.play()
+	$AnimationPlayer.play("Milina")
+
+func _on_d3_pressed():
+	$Select.play()
+	$AnimationPlayer.play("Jade")
+
+func _on_d4_pressed():
+	$Select.play()
+	$AnimationPlayer.play("Kitana")
+
+func _on_d5_pressed():
+	$Select.play()
+	$AnimationPlayer.play("Sindel")
+
+func _on_d6_pressed():
+	$Select.play()
+	$AnimationPlayer.play("Tanya")
+
+func _on_d7_pressed():
+	$Select.play()
+	$AnimationPlayer.play("Frost")
+
+func _on_d8_pressed():
+	$Select.play()
+	$AnimationPlayer.play("Cetrion")
+
+func _on_d9_pressed():
+	$Select.play()
+	$AnimationPlayer.play("Skarlet")
+
+func _on_d10_pressed():
+	$Select.play()
+	$AnimationPlayer.play("Ferra")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name=="Sonya":
+		get_tree().change_scene("res://Scenes/Sonya/Sonya.tscn")
+	elif anim_name=="Milina":
+		get_tree().change_scene("res://Scenes/Milina/Milina.tscn")
+	elif anim_name=="Jade":
+		get_tree().change_scene("res://Scenes/Jade/Jade.tscn")
+	elif anim_name=="Kitana":
+		get_tree().change_scene("res://Scenes/Kitana/Kitana.tscn")
+	elif anim_name=="Sindel":
+		get_tree().change_scene("res://Scenes/Sindel/Sindel.tscn")
+	elif anim_name=="Tanya":
+		get_tree().change_scene("res://Scenes/Tanya/Tanya.tscn")
+	elif anim_name=="Frost":
+		get_tree().change_scene("res://Scenes/Frost/Frost.tscn")
+	elif anim_name=="Cetrion":
+		get_tree().change_scene("res://Scenes/Cetrion/Cetrion.tscn")
+	elif anim_name=="Skarlet":
+		get_tree().change_scene("res://Scenes/Skarlet/Skarlet.tscn")
+	elif anim_name=="Ferra":
+		get_tree().change_scene("res://Scenes/Ferra/Ferra.tscn")
---
+++ пример/prj/global.gd
@@ -0,0 +1,44 @@
+extends Node
+
+var Sonya_life:int=300
+var Milina_life:int=500
+var Jade_life:int=800
+var Kitana_life:int=1000
+var Sindel_life:int=1200
+var Tanya_life:int=1500
+var Frost_life:int=1800
+var Cetrion_life:int=2000
+var Skarlet_life:int=2200
+var Ferra_life:int=2400
+
+var Sonya_max_life:int=300
+var Milina_max_life:int=500
+var Jade_max_life:int=800
+var Kitana_max_life:int=1000
+var Sindel_max_life:int=1200
+var Tanya_max_life:int=1500
+var Frost_max_life:int=1800
+var Cetrion_max_life:int=2000
+var Skarlet_max_life:int=2200
+var Ferra_max_life:int=2400
+
+var Milina_disabled=true
+var Jade_disabled=true
+var Kitana_disabled=true
+var Sindel_disabled=true
+var Tanya_disabled=true
+var Frost_disabled=true
+var Cetrion_disabled=true
+var Skarlet_disabled=true
+var Ferra_disabled=true
+
+var step:int=1
+
+var score:int=0
+
+var mass_add=[100,300,500,1000,2000,3000,5000]
+
+
+func _ready():
+	pass # Replace with function body.
+
